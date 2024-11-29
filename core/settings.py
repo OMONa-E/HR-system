b5245393e892f89ba48d7b283c95155722465688
Omona Emmanuel <omonaemman@gmail.com>
2024-11-29 21:21:29 +0300

    updated settings

--- core/settings.py
+++ core/settings.py
@@ -22,11 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c*wwy+a^+8yn)s4v%1ct)7wsi%^!u6wxv25h$=z4paorynk0t1'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
@@ -140,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
