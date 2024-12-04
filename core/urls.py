4b7c7d8fae0fd8a489755801b65ee2a4793ca177
Omona Emmanuel <omonaemman@gmail.com>
2024-12-04 11:16:07 +0300

    updated core urls

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,20 @@ urlpatterns = [
     path('api/attendance/', include('applications.attendance.urls')),
     path('api/leave_management/', include('applications.leave_management.urls')),
     path('api/reporting/', include('applications.reporting.urls')),
+]
+
+# Auth URLs
+# ---------------------------------------
+urlpatterns += [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
+# Passwoerd Reset URLs
+# ---------------------------------------
+urlpatterns += [
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
